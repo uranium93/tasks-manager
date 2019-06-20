8f05bf0b4ff660287b6b65d9e9e97fcca193d090
uranium93 <19hanafi93@gmail.com>
2019-06-20 09:36:03 +0100

    init commit

---
+++ mongodb.js
@@ -0,0 +1,18 @@
+// CRUD create read update delete
+
+const { MongoClient, ObjectID } = require('mongodb')
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!')
+    }
+
+
+    
+    const db = client.db(databaseName)
+    
+    
+})
