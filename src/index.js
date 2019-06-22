e3c6321a3e86018ab77846bf9b56f6a1ecfe9238
uranium93 <19hanafi93@gmail.com>
2019-06-22 10:51:30 +0100

    add get endpoint to get tasks

--- src/index.js
+++ src/index.js
@@ -27,14 +27,14 @@ app.post('/tasks',(req,res)=>{
 })
 
 app.get('/users/:id',(req,res)=>{
-    const _id=req.param
-    Users.find({"_id":_id}).then((user)=>{
+    const _id=req.params.id
+    Users.findById({_id}).then((user)=>{
         if(!user){
-            return res.status(404)
+            return res.status(404).send()
         }
         res.send(user)
     }).catch((e)=>{
-        res.status(500)
+        res.status(500).send(e)
     })
 })
 
@@ -47,6 +47,25 @@ app.get('/users',(req,res)=>{
     })
 })
 
+app.get('/tasks',(req,res)=>{
+    Tasks.find({}).then((task)=>{
+        res.status(201).send(task)
+    }).catch((e)=>{
+        res.status(500).send(e)
+    })
+})
+
+app.get('/tasks/:id',(req,res)=>{
+    const _id=req.params.id
+    Tasks.findById(_id).then((task)=>{
+        if(!task){
+            return res.status(404).send('no task found !')
+        }
+        res.status(201).send(task)
+    }).catch((e)=>{
+        res.status(500).send(e)
+    })
+})
 
 app.listen(port ,()=>{
     console.log('server is running on port 3000')
