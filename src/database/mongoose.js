c4231e26e12df0928fb6b88371ef2eeb780a4490
uranium93 <19hanafi93@gmail.com>
2019-06-22 06:10:22 +0100

    add tasks model to database

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -14,7 +14,7 @@ const User=  mongoose.model('User',{
     }
 })
 
-const newUser = new User({
+/* const newUser = new User({
     name:'abdou',
     age:26
 })
@@ -24,4 +24,25 @@ newUser.save().then((result)=>{
 
 }).catch((error)=>{
     console.log("Faild! ")
+}) */
+
+const Tasks = mongoose.model('Tasks',{
+    task:{
+        type:String
+    },
+    status:{
+        type:Boolean
+    }
 })
+
+const newTask = new Tasks({
+    name:"add credit dard to the project",
+    status:false
+})
+
+newTask.save().then((result)=>{
+    console.log('Succes',result)
+}).catch((error)=>{
+    console.log('Faild!',error.value)
+})
+
