9525e3748604dd4299ed46073b6be27750981788
uranium93 <19hanafi93@gmail.com>
2019-06-22 07:07:10 +0100

    add sanitization,add password and the validation

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -10,18 +10,33 @@ const User=  mongoose.model('User',{
     name:{
         type:String,
         required:true,
+        trim:true
     },
     email:{
         type:String,
         required:true,
+        trim:true,
+        lowercase:true,
         validate(value){
             if(!validator.isEmail(value)){
                 throw new Error('invalid email ') 
             }
         }
     },
+    password:{
+        type:String,
+        required:true,
+        trim:true,
+        minlength:6,
+        validate(value){
+            if(value.includes('password')){
+                throw new Error('dont use the string "passwrod" in your password')
+            }
+        }
+    },
     age:{
         type:Number,
+        dafault:0,
         validate(value){
             if(value<0){
                 throw new Error('age must be positive value')
@@ -31,16 +46,17 @@ const User=  mongoose.model('User',{
 })
 
 const newUser = new User({
-    name:'yahya',
-    email:'yahya@hotmail.com',
-    age:26
+    name:'heithem         ',
+    email:'yaHYa@hotmail.com',
+    password:'24Guelma'
+    
 })
 
 newUser.save().then((result)=>{
     console.log("Succes",result)
 
 }).catch((error)=>{
-    console.log("Faild! ")
+    console.log("Faild! ",error)
 })
 
 const Tasks = mongoose.model('Tasks',{
